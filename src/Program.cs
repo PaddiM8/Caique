357e50765686977a451ddb0a7c667ffe7091b69b
PaddiM8 <paddim8@pm.me>
2020-09-07 19:10:34 +0200

    created C# project

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Caique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
