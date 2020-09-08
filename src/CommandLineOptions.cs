014d6d4567354bd5371541d58080447881bb8593
PaddiM8 <paddim8@pm.me>
2020-09-08 14:53:47 +0200

    added command line parsing

---
+++ src/CommandLineOptions.cs
@@ -0,0 +1,17 @@
+using System;
+using CommandLine;
+
+namespace Caique
+{
+    class CommandLineOptions
+    {
+        [Option("ast", Required = false, HelpText = "Print the Abstract Syntax Tree")]
+        public bool PrintAst { get; set; }
+
+        [Option("tokens", Required = false, HelpText = "Print the token list made by the lexer")]
+        public bool PrintTokens { get; set; }
+
+        [Value(0, MetaName = "input file", HelpText = "Path to the Caique input file")]
+        public string InputFile { get; set; }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.IO;
+using CommandLine;
+using CommandLine.Text;
 
 namespace Caique
 {
     class Program
     {
+        public static CommandLineOptions Options { get; private set; }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Create the cli parser with HelpWriter disabled,
+            // since a custom help text will be used
+            var cliParser = new CommandLine.Parser(with => with.HelpWriter = null);
+            var cliParserResult = cliParser.ParseArguments<CommandLineOptions>(args);
+
+            // Use the command line options
+            cliParserResult.WithParsed(options =>
+            {
+                Options = options;
+
+                // Do something with the options...
+                new Compilation(File.ReadAllText(options.InputFile));
+            });
+
+            // Show the help text if there was an error
+            cliParserResult.WithNotParsed(errors => PrintHelp(cliParserResult));
+        }
+
+        private static void PrintHelp(ParserResult<CommandLineOptions> result)
+        {
+            var helpText = HelpText.AutoBuild(result, help =>
+            {
+                help.AdditionalNewLineAfterOption = false;
+                help.Copyright = "";
+
+                return help;
+            }, e => e);
+
+            Console.WriteLine(helpText);
         }
     }
 }
