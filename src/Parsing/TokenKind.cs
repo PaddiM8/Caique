53fd09a6474bfd8cba4e390af48386168daaffe4
PaddiM8 <paddim8@pm.me>
2020-09-09 14:04:52 +0200

    added TokenKind members

--- src/Parsing/TokenKind.cs
+++ src/Parsing/TokenKind.cs
@@ -1,6 +1,20 @@
 namespace Caique.Parsing
 {
-    enum TokenKind
+    public enum TokenKind
     {
+        // Maths
+        Plus, Minus, Star, Slash, 
+
+        // Comparison
+        Equals, BangEquals, EqualsEquals, MoreOrEquals, LessOrEquals,
+
+        // Parenthesis and brackets
+        OpenParenthesis, ClosedParenthesis, OpenSquareBracket, ClosedSquareBracket, OpenAngleBracket, ClosedAngleBracket,
+
+        // Variable length
+        Identifier, NumberLiteral, StringLiteral,
+
+        // Other
+        Unknown,
     }
 }
